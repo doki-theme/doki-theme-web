fb6d66097950df7a776f3164cf6211084e4bd8ae
DATBOI <alexsimons9999@gmail.com>
2020-06-16 18:22:32 -0500

    Yay! I can change the background color now :D

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,13 @@
-
-function polling() {
-    console.log('polling');
-    setTimeout(polling, 1000 * 30);
-}
-
-polling();
-
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({color: '#3aa757'}, () => {
+    console.log('The color is green.');
+  });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'developer.chrome.com'},
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
