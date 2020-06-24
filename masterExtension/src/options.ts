a174689b63acf3d5e207fb9ffe63bc7d0085db79
DATBOI <alexsimons9999@gmail.com>
2020-06-24 18:20:38 -0500

    Fancy background

---
+++ masterExtension/src/options.ts
@@ -0,0 +1,42 @@
+import DokiThemeDefinitions from "./DokiThemeDefinitions";
+
+function drawBackground() {
+  const backgroundCanvas = document.getElementById(
+    "backgroundImage"
+  )!!;
+  // @ts-ignore
+  const ctx = backgroundCanvas.getContext("2d");
+  if (!ctx) return;
+  const mainCanvas = document.getElementById("main");
+  if (!mainCanvas) {
+    return;
+  }
+
+  const boundingRect = mainCanvas.getBoundingClientRect();
+  const w = boundingRect.width;
+  const h = boundingRect.height;
+
+  backgroundCanvas.setAttribute('width',String(w));
+  backgroundCanvas.setAttribute('height', String(h))
+
+
+  ctx.clearRect(0, 0, w, h);
+  ctx.beginPath();
+  ctx.moveTo(0, h * 0.85);
+  ctx.quadraticCurveTo(w / 1.85, h, w, 0);
+  ctx.lineTo(w, h);
+  ctx.lineTo(0, h);
+  ctx.fillStyle = "#4c2e5c";
+  ctx.strokeStyle = "#4c2e5c";
+  ctx.fill();
+  ctx.closePath();
+  ctx.stroke();
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  drawBackground()
+  window.addEventListener('resize', ()=>{
+    drawBackground()
+  });
+})
