3329ab0a4f4d8eb86808e572abbed28e6bcabcdd
DATBOI <alexsimons9999@gmail.com>
2020-06-24 05:09:58 -0500

    Can change scrollbarz and selection color

--- masterExtension/src/background.ts
+++ masterExtension/src/background.ts
@@ -3,3 +3,13 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log('The color is green.');
   });
 });
+
+chrome.tabs.onUpdated.addListener(((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    const color = 'aoeu'
+    chrome.tabs.query({currentWindow: true, active: true}, ([{id}]) => {
+      console.log(`Sending color message`);
+      chrome.tabs.sendMessage(id || 69, {color})
+    })
+  }
+}));
--- masterExtension/src/content_script.ts
+++ masterExtension/src/content_script.ts
@@ -1,10 +1,29 @@
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    if (msg.color) {
-        document.body.style.backgroundColor = msg.color;
-        document.body
-        sendResponse(`Change color to ${msg.color}`);
-    } else {
-        sendResponse('Color message is none.');
-    }
+  if (msg.color) {
+    const styles = `
+      ::-moz-selection { background: #a73035; color: #E2CB14; }
+      ::selection { background: #a73035; color: #E2CB14; }
+
+      *::-webkit-scrollbar {
+        width: 0.5em;
+      }
+
+      *::-webkit-scrollbar-track {
+        -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.00);
+      }
+
+      *::-webkit-scrollbar-thumb {
+        background-color: #a73035;
+     } 
+        `
+    const styleSheet = document.createElement("style");
+    styleSheet.type = "text/css"
+    styleSheet.innerText = styles;
+    document.head.appendChild(styleSheet)
+
+    sendResponse(`Change color to ${msg.color}`);
+  } else {
+    sendResponse('Color message is none.');
+  }
 });
 
