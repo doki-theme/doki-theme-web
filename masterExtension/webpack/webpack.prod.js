daed786b4c938a57aed7205b7b3bb3b57f03149d
DATBOI <alexsimons9999@gmail.com>
2020-06-25 05:06:00 -0500

    Installable extension

--- masterExtension/webpack/webpack.prod.js
+++ masterExtension/webpack/webpack.prod.js
@@ -1,6 +1,11 @@
 const merge = require('webpack-merge');
+const path = require('path');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'production'
+  mode: 'production',
+  output: {
+    path: path.join(__dirname, '../installable_extension/js'),
+    filename: '[name].js'
+  }
 });
