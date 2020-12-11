3984698e56d5d38351f0ee5d6bbe49c06e3b8306
ZimCodes <23222943+ZimCodes@users.noreply.github.com>
2020-12-10 19:49:04 -0800

    This is the base functionality of Doki Theme.

--- background.js
+++ background.js
@@ -1,28 +1,51 @@
-let waifu = "";
-function newTabPage(tab){
-	if(tab.url.includes("about:privatebrowsing")
-	||tab.url.includes("about:home")
-	||tab.url.includes("about:newtab")){
-		browser.tabs.update(
-			tab.id,
-			{
-				active:true,
-				url:"/waifus/index.html"
-			}
-		);
-		browser.tabs.sendMessage(
-			tab.id,
-			{waifu:waifu}
-		);
-		console.log("Background: new Tab");
+let storage = {
+	waifu: ""
+}
+function setTheme(json){
+	fetch(json)
+		.then((res)=>{
+			return res.json();
+		})
+		.then((theme)=>{
+			browser.theme.update(theme);
+		});
+	console.log("Theme set!");
+}
+function setPage(tabID,page){
+	browser.tabs.update(
+		tabID,
+		{
+			active:true,
+			url:page
+		}
+	);
+}
+function activateTheme(tabID){
+	console.log("Activate theme!");
+	console.log(`waifu: ${storage.waifu}`);
+	switch(storage.waifu) {
+		case "Ishtar-Light":
+			setPage(tabID,"/waifus/ishtar/dark/index.html");
+			setTheme("/waifus/ishtar/dark/theme.json");
+			break;
+		case "Ishtar-Dark":
+			break;
+		default:
+			console.log("RESET!!");
+			browser.theme.reset();
+			break;
 	}
 }
-function getWaifu(message){
-	if(waifu !== message.waifu){
-		waifu = message.waifu;
-		console.log("Background: New Waifu");
+function newTabPage(tab){
+	const pagesToEffect = tab.url.includes("about:privatebrowsing")
+	||tab.url.includes("about:home")
+	||tab.url.includes("about:newtab");
+	if(pagesToEffect){
+		activateTheme(tab.id);
 	}
-	
+}
+function getWaifu(msg){
+	storage.waifu = msg.waifu;
 }
 browser.runtime.onMessage.addListener(getWaifu);
 browser.tabs.onCreated.addListener(newTabPage);
--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,7 @@
 function chooseWaifu(e){
 	const waifuChoice = e.target.value;
 	browser.runtime.sendMessage({waifu:waifuChoice});
+	console.log(`Waifu: ${waifuChoice}`);
 	console.log("Popup: Waifu Chosen!");
 }
 document.querySelector("select").addEventListener("change",chooseWaifu,true);
