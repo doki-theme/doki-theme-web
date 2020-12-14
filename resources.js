8e7983686f12cef7513361baf8704d2ccf6bc292
ZimCodes <23222943+ZimCodes@users.noreply.github.com>
2020-12-14 04:29:52 -0800

    Adds more waifu themes to complete; 10 waifus can now be set as background

--- resources.js
+++ resources.js
@@ -8,8 +8,17 @@ class WaifuThemes{
         this.themes = {
 			aquaDark: new Theme("/waifus/aqua/dark/theme.json","/waifus/aqua/dark/index.html"),
 			asunaLight: new Theme("/waifus/asuna/light/theme.json","/waifus/asuna/light/index.html"),
-            ishtarDark: new Theme("/waifus/ishtar/dark/theme.json","/waifus/ishtar/dark/index.html"),
-            ishtarLight: new Theme("/waifus/ishtar/light/theme.json","/waifus/ishtar/light/index.html")
+			beatrice: new Theme("/waifus/beatrice/theme.json","/waifus/beatrice/index.html"),
+			darknessDark: new Theme("/waifus/darkness/dark/theme.json","/waifus/darkness/dark/index.html"),
+            darknessLight: new Theme("waifus/darkness/light/theme.json","/waifus/darkness/light/index.html"),
+			emiliaDark: new Theme("/waifus/emilia/dark/theme.json","/waifus/emilia/dark/index.html"),
+			emiliaLight: new Theme("/waifus/emilia/light/theme.json","/waifus/emilia/light/index.html"),
+			hatsuneMiku:new Theme("/waifus/hatsune-miku/theme.json","waifus/hatsune-miku/index.html"),
+			ishtarDark: new Theme("/waifus/ishtar/dark/theme.json","/waifus/ishtar/dark/index.html"),
+            ishtarLight: new Theme("/waifus/ishtar/light/theme.json","/waifus/ishtar/light/index.html"),
+			kannaDark: new Theme("/waifus/kanna/dark/theme.json","/waifus/kanna/dark/index.html"),
+			konataLight: new Theme("/waifus/konata/light/theme.json","/waifus/konata/light/index.html"),
+			megumin: new Theme("/waifus/megumin/theme.json","/waifus/megumin/index.html"),
         }
     }
     /*Retrieve the custom Waifu Tab page resource*/
