6cee46fa658a36fbe71e69c1358d624369c702b3
ZimCodes <23222943+ZimCodes@users.noreply.github.com>
2020-12-16 20:50:24 -0800

    Adds 13 more waifu themes;Adds a permissions file, which explains the purpose of each permission needed to use the extension

--- resources.js
+++ resources.js
@@ -24,6 +24,21 @@ class WaifuThemes{
 			misatoKatsuragi: new Theme("/waifus/misato-katsuragi/theme.json","/waifus/misato-katsuragi/index.html"),
 			monikaDark: new Theme("/waifus/monika/dark/theme.json","/waifus/monika/dark/index.html"),
 			monikaLight: new Theme("/waifus/monika/light/theme.json","/waifus/monika/light/index.html"),
+			natsukiHappyDark: new Theme("/waifus/natsuki/dark/theme.json","/waifus/natsuki/dark/happy/index.html"),
+			natsukiSadDark: new Theme("/waifus/natsuki/dark/theme.json","/waifus/natsuki/dark/sad/index.html"),
+			natsukiHappyLight: new Theme("/waifus/natsuki/light/theme.json","/waifus/natsuki/light/happy/index.html"),
+			natsukiSadLight: new Theme("/waifus/natsuki/light/theme.json","/waifus/natsuki/light/sad/index.html"),
+			ram: new Theme("/waifus/ram/theme.json","/waifus/ram/index.html"),
+			rem: new Theme("/waifus/rem/theme.json","/waifus/rem/index.html"),
+			rias: new Theme("/waifus/rias/theme.json","/waifus/rias/index.html"),
+			roryMercury: new Theme("/waifus/rory-mercury/theme.json","/waifus/rory-mercury/index.html"),
+			ryuko: new Theme("/waifus/ryuko/theme.json","/waifus/ryuko/index.html"),
+			satsuki: new Theme("/waifus/satsuki/theme.json","/waifus/satsuki/index.html"),
+			sayoriDark: new Theme("/waifus/sayori/dark/theme.json","/waifus/sayori/dark/index.html"),
+			sayoriLight: new Theme("/waifus/sayori/light/theme.json","/waifus/sayori/light/index.html"),
+			tohsakaRin: new Theme("/waifus/tohsaka-rin/theme.json","/waifus/tohsaka-rin/index.html"),
+			yuriDark: new Theme("/waifus/yuri/dark/theme.json","/waifus/yuri/dark/index.html"),
+			yuriLight: new Theme("/waifus/yuri/light/theme.json","/waifus/yuri/light/index.html"),
         }
     }
     /*Retrieve the custom Waifu Tab page resource*/
