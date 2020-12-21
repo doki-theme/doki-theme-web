80cb1f8c130eca9fc62e27305b01886dde1d21c3
DATBOI <alexsimons9999@gmail.com>
2020-12-21 11:52:37 -0600

    Added coloring to firefox template

--- buildSrc/src/BuildThemes.ts
+++ buildSrc/src/BuildThemes.ts
@@ -37,7 +37,7 @@ const chromeDefinitionDirectoryPath = path.resolve(
   "definitions"
 );
 
-
+//
 function walkDir(dir: string): Promise<string[]> {
   const values: Promise<string[]>[] = fs.readdirSync(dir)
     .map((file: string) => {
